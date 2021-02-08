7df6e56b1d1391d3f5d4cc820c82631d81bd2b34
Hayk Ghazaryan <hghazaryan@servicetitan.com>
2021-02-08 19:14:19 +0400

    Context usage

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -5,31 +5,24 @@ import Button from "@material-ui/core/Button";
 import { AuthContext } from "../../context";
 
 export default class Login extends React.Component {
-
   render() {
     return (
       <AuthContext.Consumer>
-        {
-          ({ login }) => {
-            return (
-              <div>
-                <Typography paragraph>The 'Login' Page comming soon...</Typography>
-                <Button
-                  variant="contained"
-                  onClick={login}
-                >
-                  <Link to="/admin">Login</Link>
-                </Button>
-                <Link
-                  to="/user"
-                  onClick={login}
-                >
-                  User
-                </Link>
-              </div>
-            )
-          }
-        }
+        {({ login }) => {
+          return (
+            <div>
+              <Typography paragraph>
+                The 'Login' Page comming soon...
+              </Typography>
+              <Button variant="contained" onClick={login}>
+                <Link to="/admin">Login to admin area</Link>
+              </Button>
+              <Button variant="contained" onClick={login}>
+                <Link to="/user">Login to user area</Link>
+              </Button>
+            </div>
+          );
+        }}
       </AuthContext.Consumer>
     );
   }
--- src/protected-route.js
+++ src/protected-route.js
@@ -1,29 +1,33 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { AuthContext } from "./context";
 
 export const ProtectedRoute = ({ component: Component, ...rest }) => {
-  const authContext = useContext(AuthContext);
   return (
-    <Route
-      {...rest}
-      render={(props) => {
-        if (authContext.isLoggedIn) {
-          return <Component {...props} />;
-        } else {
-          return (
-            <Redirect
-              to={{
-                pathname: "/",
-                state: {
-                  from: props.location,
-                },
-              }}
-            />
-          );
-        }
-      }
-      }
-    />
+    <AuthContext.Consumer>
+      {({ isLoggedIn }) => {
+        return (
+          <Route
+            {...rest}
+            render={(props) => {
+              if (isLoggedIn) {
+                return <Component {...props} />;
+              } else {
+                return (
+                  <Redirect
+                    to={{
+                      pathname: "/",
+                      state: {
+                        from: props.location,
+                      },
+                    }}
+                  />
+                );
+              }
+            }}
+          />
+        );
+      }}
+    </AuthContext.Consumer>
   );
 };
