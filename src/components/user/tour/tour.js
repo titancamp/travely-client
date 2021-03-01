fd32ccd7d7ea6b06c2a9b5f7046349543013976c
ArturVah <artur.vaganyan96@gmail.com>
2021-03-01 18:54:40 +0400

    added css for tour page create tour button

--- src/components/user/tour/tour.js
+++ src/components/user/tour/tour.js
@@ -3,6 +3,8 @@ import Button from "@material-ui/core/Button";
 import { DataGrid } from '@material-ui/data-grid';
 import { TOUR_ROWS, TOUR_COLUMNS } from './utils/constants';
 import CreateTour from "./create-tour";
+import AddIcon from '@material-ui/icons/Add';
+import "./tour.css"
 
 export default class Tour extends React.Component {
   constructor(props) {
@@ -31,7 +33,11 @@ export default class Tour extends React.Component {
   render() {
     return (
         <>
-          <Button variant="contained" onClick={this.handleCreateModalToggle}> Create Tour </Button>
+          <div className="create-tour-button">
+            <AddIcon className="create-tour-icon" onClick={this.handleCreateModalToggle}/>
+            <Button variant="contained" color="primary" onClick={this.handleCreateModalToggle}> Create Tour </Button>
+          </div>
+
           <DataGrid rows={this.state.tourData} columns={TOUR_COLUMNS} autoHeight autoPageSize />
           <CreateTour isOpen={this.state.isCreateModalOpen} handleCreateModalToggle={this.handleCreateModalToggle}/>
         </>
