b37816badbcc128a82fdc94f57800dd9a1715a8c
Tadevos Stepanyan <57753967+TadevosStepanyan@users.noreply.github.com>
2021-03-25 14:43:10 +0400

    change ApiClient baseURL

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const ApiClient = axios.create({
-  baseURL: "https://api.example.com",
+  // baseURL: "https://api.example.com",
+  baseURL: "http://localhost:5000",
   headers: {
     // Authorization: "Bearer " + jwtToken,
-    // "Content-Type": "application/json",
+    "Content-Type": "application/json",
   },
   //    ... rest of configuration
 });
