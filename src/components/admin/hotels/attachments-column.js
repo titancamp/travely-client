0ade80089acde55fedbbb29c04cb20ae456fae0f
ghazaryanvardan0397 <ghazaryanvardan0397@gmail.com>
2021-03-26 21:27:47 +0400

    added api calls (get and delete), and style fixing

--- src/components/admin/hotels/attachments-column.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Attachments from "../../common/attachments";
-
-const AttachmentsColumn = (attachments) => (
-  <Attachments attachments={attachments.value} />
-);
-
-export default AttachmentsColumn;
---
+++ src/components/common/delete-item.js
@@ -0,0 +1,64 @@
+import React, {useState} from 'react';
+import ConfirmDialog from "../user/guest/tourists/component/ConfirmDialog";
+import {Close as CloseIcon} from "@material-ui/icons";
+import Button from "@material-ui/core/Button";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import {ManageHotelContext} from "../../store/context";
+
+const useStyles = makeStyles((theme) => ({
+    button: {
+        color: theme.palette.grey[700],
+    },
+}));
+
+const DeleteItem = (props) => {
+    const itemId = props.row.id;
+
+    const [confirmDialog, setConfirmDialog] = useState({
+        isOpen: false,
+        title: "",
+    });
+    const classes = useStyles();
+    const onDelete = () => {
+        setConfirmDialog({
+            ...confirmDialog,
+            isOpen: false,
+        });
+    };
+
+    return (
+        <div>
+            <ManageHotelContext.Consumer>
+                {
+                    ({deleteHandler}) => {
+                        return (
+                            <div>
+                                <Button
+                                    className={classes.button}
+                                    onClick={() => {
+                                        setConfirmDialog({
+                                            isOpen: true,
+                                            title: "Are you sure to delete this item",
+                                            onConfirm: () => {
+                                                deleteHandler(itemId);
+                                                onDelete();
+                                            },
+                                        });
+                                    }}
+                                >
+                                    <CloseIcon fontSize="small"/>
+                                </Button>
+                                <ConfirmDialog
+                                    confirmDialog={confirmDialog}
+                                    setConfirmDialog={setConfirmDialog}
+                                />
+                            </div>
+                        );
+                    }
+                }
+            </ManageHotelContext.Consumer>
+        </div>
+    );
+};
+
+export default DeleteItem;
