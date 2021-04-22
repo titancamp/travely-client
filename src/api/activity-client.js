a1c147e551a79d2288fe923ca7f46230e7e4eeee
vyegiazaryan <vyegiazaryan@gmail.com>
2021-04-22 17:41:54 +0400

    implemented activity client

--- src/api/activity-client.js
+++ src/api/activity-client.js
@@ -1,14 +1,26 @@
+import ApiClient from "./api";
+
 const ActivityClient = {
   addActivity: async function (data) {
-    return await setTimeout(() => {
-      return data;
-    }, 0);
+    return await ApiClient.post('/api/v1/service', data)
+      .then(response => response.data);
   },
   editActivity: async function (data) {
-    return await setTimeout(() => {
-      return data;
-    }, 0);
+    return await ApiClient.put('/api/v1/service', data)
+      .then(response => response.data);
   },
+  deleteActivity: async function (id) {
+    return await ApiClient.delete(`/api/v1/service${id}`)
+      .then(response => response.data);
+  },
+  getActivities: async function () {
+    return await ApiClient.get('/api/v1/service')
+      .then(response => response.data);
+  },
+  getActivityTypes: async function (nameSearch) {
+    return await ApiClient.get(`/api/v1/service/activitytype?activityTypeName=${nameSearch || ''}`)
+      .then(response => response.data);
+  }
 };
 
 export default ActivityClient;
