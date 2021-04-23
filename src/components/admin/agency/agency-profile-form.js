53c3964a9d2111bccda959f770b66340917c798c
sasuntitan <sstepanyan@servicetitan.com>
2021-04-23 11:50:32 +0400

    agency profile

--- src/components/admin/agency/agency-profile-form.js
+++ src/components/admin/agency/agency-profile-form.js
@@ -3,7 +3,15 @@ import React, { useRef, useCallback, useEffect, useState } from "react";
 import { Formik } from "formik";
 import * as yup from "yup";
 
-import { Grid, TextField, Button, Box, Divider, CircularProgress, Backdrop } from "@material-ui/core";
+import {
+  Grid,
+  TextField,
+  Button,
+  Box,
+  Divider,
+  CircularProgress,
+  Backdrop,
+} from "@material-ui/core";
 
 import AgencyClient from "../../../api/agency-client";
 import FileClient from "../../../api/file-client";
@@ -11,17 +19,20 @@ import FileClient from "../../../api/file-client";
 import { AuthContext } from "../../../store/context";
 import ImageOutlinedIcon from "@material-ui/icons/ImageOutlined";
 import LogoUpload from "../agency/logo-upload";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   backdrop: {
     zIndex: theme.zIndex.drawer + 1,
-    color: '#fff',
+    color: "#fff",
   },
 }));
 
 const AgencyProfileSchema = yup.object().shape({
-  phoneNumber: yup.number().typeError("Phone must be a number").required("Required"),
+  phoneNumber: yup
+    .number()
+    .typeError("Phone must be a number")
+    .required("Required"),
 });
 
 export default function AgencyProfileForm() {
@@ -33,23 +44,25 @@ export default function AgencyProfileForm() {
   const authContext = JSON.parse(localStorage.getItem("AuthContext"));
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => { 
+  useEffect(() => {
     const getAgency = async () => {
-      await AgencyClient.get().then((response) => {
-        const data = response.data;
-        formik.current.setValues({
-          phoneNumber: data.phoneNumber,
-          address: data.address,
-          logoFile: data.logoFile,
-          id: data.id,
+      await AgencyClient.get()
+        .then((response) => {
+          const data = response.data;
+          formik.current.setValues({
+            name: data.name,
+            phoneNumber: data.phoneNumber,
+            address: data.address,
+            logoFile: data.logoFile,
+            id: data.id,
+          });
         })
-      })
-      .catch((error)=>{
-        // TODO: Add error message
-      })
-      .finally(()=>{
-        setLoading(false);
-      });
+        .catch((error) => {
+          // TODO: Add error message
+        })
+        .finally(() => {
+          setLoading(false);
+        });
     };
     setLoading(true);
     getAgency();
@@ -64,10 +77,10 @@ export default function AgencyProfileForm() {
   }, []);
 
   const logoUploadMemoizedCallback = useCallback(
-    ({ values }) =>
-      (<Box>
+    ({ values }) => (
+      <Box>
         <input
-         name="logoFile"
+          name="logoFile"
           type="file"
           ref={logoInput}
           onChange={logoInputChangeCallback}
@@ -78,41 +91,46 @@ export default function AgencyProfileForm() {
           select logo
         </Button>
         <Divider />
-        <LogoUpload file={values.file} logoId={values.logoFile} agencyId={authContext.agencyId} />
-      </Box>),
+        <LogoUpload
+          file={values.file}
+          logoId={values.logoFile}
+          agencyId={authContext.agencyId}
+        />
+      </Box>
+    ),
     [logoInput, logoInputChangeCallback, authContext.agencyId]
   );
 
   const getAgencyProfileSubmitData = (values, fileId) => {
     return [
       {
-      "op": "replace",
-      "path": "/phoneNumber",
-      "value": values.phoneNumber,
+        op: "replace",
+        path: "/phoneNumber",
+        value: values.phoneNumber,
       },
       {
-      "op": "replace",
-      "path": "/address",
-      "value": values.address
+        op: "replace",
+        path: "/address",
+        value: values.address,
       },
       {
-      "op": "replace",
-      "path": "/logoFile",
-      "value": fileId,
-      }
-    ]
+        op: "replace",
+        path: "/logoFile",
+        value: fileId,
+      },
+    ];
   };
 
   const onFormSubmit = async (values, ownerId) => {
     const formData = new FormData();
-    formData.append("file", logoFormik.current.file);
+    formData.append("file", logoFormik.current.values.file);
     let fileId = "";
 
-    await FileClient.upload(ownerId, formData).then((response) => {
-      fileId = response.data.data;
-    })
-    .catch((error) => {
-    }); // TODO: Add error message
+    await FileClient.upload(+localStorage.getItem("agencyId"), formData)
+      .then((response) => {
+        fileId = response.data.data;
+      })
+      .catch((error) => {}); // TODO: Add error message
 
     AgencyClient.update(getAgencyProfileSubmitData(values, fileId))
       .then((result) => {
@@ -134,24 +152,24 @@ export default function AgencyProfileForm() {
       <Backdrop className={classes.backdrop} open={loading}>
         <CircularProgress color="inherit" />
       </Backdrop>
-      
+
       <AuthContext.Consumer>
         {({ ownerId }) => {
-          return(
+          return (
             <Formik
-              innerRef={ (ref) => (formik.current = ref) }
+              innerRef={(ref) => (formik.current = ref)}
               initialValues={{
+                name: "",
                 phoneNumber: "",
                 address: "",
                 logoFile: "",
                 id: "",
               }}
-              validationSchema={ AgencyProfileSchema }
-              onSubmit={ async (values, { setSubmitting }) => {
-                onFormSubmit(values, ownerId)
-                  .finally(() => {
-                    setSubmitting();
-                  });
+              validationSchema={AgencyProfileSchema}
+              onSubmit={async (values, { setSubmitting }) => {
+                onFormSubmit(values, ownerId).finally(() => {
+                  setSubmitting();
+                });
               }}
             >
               {({
@@ -165,6 +183,23 @@ export default function AgencyProfileForm() {
               }) => (
                 <Grid item xs={4}>
                   <form onSubmit={handleSubmit}>
+                    <Box m={2}>
+                      <TextField
+                        type="text"
+                        name="name"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.name}
+                        label="Name"
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                        disabled
+                      />
+                      <Box color="error.main">
+                        {errors.address && touched.address && errors.address}
+                      </Box>
+                    </Box>
                     <Box m={2}>
                       <TextField
                         type="text"
@@ -195,18 +230,26 @@ export default function AgencyProfileForm() {
                         fullWidth
                       />
                       <Box color="error.main">
-                        {errors.phoneNumber && touched.phoneNumber && errors.phoneNumber}
+                        {errors.phoneNumber &&
+                          touched.phoneNumber &&
+                          errors.phoneNumber}
                       </Box>
                     </Box>
 
                     <Box m={2}>
-                      <Formik innerRef={logoFormik} initialValues={{ file: null, logoFile: values.logoFile }}>
+                      <Formik
+                        innerRef={logoFormik}
+                        initialValues={{
+                          file: null,
+                          logoFile: values.logoFile,
+                        }}
+                      >
                         {logoUploadMemoizedCallback}
                       </Formik>
                     </Box>
 
                     <Box m={2}>
-                      <Button 
+                      <Button
                         type="submit"
                         disabled={isSubmitting}
                         variant="contained"
