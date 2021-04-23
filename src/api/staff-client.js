57b3b4f1398e1938df6a6ea326ea4609caeae6c4
Tadevos Stepanyan <57753967+TadevosStepanyan@users.noreply.github.com>
2021-04-23 05:21:56 +0400

    small change

--- src/api/staff-client.js
+++ src/api/staff-client.js
@@ -1,20 +1,31 @@
 import ApiClient from "./api";
+import appConfig from '../app-config.json';
 
 const StaffClient = {
   get: function (id) {
-    return ApiClient.get(`/api/users/${id}`);
+    return ApiClient.get(`/api/users/${id}`, {
+      baseURL: appConfig.authURL
+    });
   },
   create: function (formData) {
-    return ApiClient.put(`/api/users`, formData);
+    return ApiClient.put(`/api/users`, formData, {
+      baseURL: appConfig.authURL
+    });
   },
   update: function (id, formData) {
-    return ApiClient.put(`/api/users/${id}`, formData);
+    return ApiClient.put(`/api/users/${id}`, formData, {
+      baseURL: appConfig.authURL
+    });
   },
   delete: function (id) {
-    return ApiClient.delete(`/api/users/${id}`);
+    return ApiClient.delete(`/api/users/${id}`, {
+      baseURL: appConfig.authURL
+    });
   },
   getAll: function () {
-    return ApiClient.get("/api/users");
+    return ApiClient.get("/api/users", {
+      baseURL: appConfig.authURL
+    });
   },
 };
 
