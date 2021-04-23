5a570cd65f0e83190872d12f8bd1f888f28ac988
Hayk Ghazaryan <hghazaryan@servicetitan.com>
2021-04-23 18:42:51 +0400

    sidebar items removed

--- src/components/admin/admin.js
+++ src/components/admin/admin.js
@@ -3,14 +3,12 @@ import React from "react";
 import ApartmentIcon from "@material-ui/icons/Apartment";
 import GroupIcon from "@material-ui/icons/Group";
 import HotelIcon from "@material-ui/icons/Hotel";
-import LockIcon from "@material-ui/icons/Lock";
 import PublicIcon from "@material-ui/icons/Public";
 
 import AgencyProfile from "./agency/agency-profile";
 import ManageStaff from "./staff/manage-staff";
 import ManageHotels from "./hotels/manage-hotels";
 import ManageActivities from "./activities/manage-activities";
-import ChangePassword from "./account/change-password";
 import { ContentArea } from "../common/content-area";
 import { Sidebar } from "../common/sidebar";
 import NotificationPane from "../common/notification-pane";
@@ -39,12 +37,6 @@ const pages = [
     path: "/admin/activities",
     icon: <PublicIcon />,
     component: ManageActivities,
-  },
-  {
-    title: "Change Password",
-    path: "/admin/password",
-    icon: <LockIcon />,
-    component: ChangePassword,
   },
 ];
 
--- src/components/admin/staff/manage-staff-form.js
+++ src/components/admin/staff/manage-staff-form.js
@@ -8,7 +8,6 @@ import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
-import GroupIcon from "@material-ui/icons/Group";
 
 import FormikInputField from "../../UI/FormikComponents/FormikInputField";
 
