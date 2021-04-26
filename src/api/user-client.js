815b4d90fe527b383aded62dcde19c8f0e72fffc
Hayk Ghazaryan <hghazaryan@servicetitan.com>
2021-04-26 18:09:35 +0400

    login flow change

--- src/api/user-client.js
+++ src/api/user-client.js
@@ -1,15 +1,20 @@
 import ApiClient from "./api";
-import appConfig from '../app-config.json';
+import appConfig from "../app-config.json";
 
 const UserClient = {
   get: function (id) {
     return ApiClient.get(`/api/users/${id}`, {
-      baseURL: appConfig.authURL
+      baseURL: appConfig.authURL,
     });
   },
   getAll: function () {
     return ApiClient.get(`/api/users`, {
-      baseURL: appConfig.authURL
+      baseURL: appConfig.authURL,
+    });
+  },
+  getCurrentUser: function () {
+    return ApiClient.get(`/api/users/current`, {
+      baseURL: appConfig.authURL,
     });
   },
 };
--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 
 import { Formik } from "formik";
 import * as yup from "yup";
 
 import Button from "@material-ui/core/Button";
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 
 import { AuthContext } from "../../store/context";
@@ -14,12 +14,16 @@ import LoginLayout from "../common/login-layout";
 import AuthClient from "../../api/auth-client";
 import AgencyClient from "../../api/agency-client";
 import UserClient from "../../api/user-client";
-import { useStyles } from './auth-style';
+import { useStyles } from "./auth-style";
 
-import FormikInputField from "../UI/FormikComponents/FormikInputField"
+import FormikInputField from "../UI/FormikComponents/FormikInputField";
+import jwtDecode from "jwt-decode";
 
 const LoginSchema = yup.object().shape({
-  username: yup.string().email("Enter a valid email").required("Email is required"),
+  username: yup
+    .string()
+    .email("Enter a valid email")
+    .required("Email is required"),
   password: yup.string().required("Password is required"),
 });
 
@@ -31,19 +35,19 @@ const Login = (props) => {
     if (error.response && error.response.status) {
       setErrors({
         message: "Email or password are incorrect",
-      })
-    }
-    else {
+      });
+    } else {
       setErrors({
         message: "Server is unreachable",
-      })
+      });
     }
   };
 
   const onSubmit = (values, login, setSubmitting, setErrors) => {
     AuthClient.login(values.username, values.password)
       .then((result) => {
-        const expiresIn = new Date(Date.now() + (result.data.expires_in * 1000));
+        const { role, sub } = jwtDecode(result.data.access_token);
+        const expiresIn = new Date(Date.now() + result.data.expires_in * 1000);
         const data = {
           loggedIn: true,
           accessToken: result.data.access_token,
@@ -51,26 +55,29 @@ const Login = (props) => {
           expiresIn,
           ownerId: null,
           email: values.username,
-          role: null,
-        }
+          role,
+        };
         localStorage.setItem("AuthContext", JSON.stringify(data));
 
-        AgencyClient.get().then((result) => {
-          localStorage.setItem("agencyId", result.data.id);
-          data.agencyId = result.data.id;
-          data.userId = result.data.ownerId;
-          UserClient.get(result.data.ownerId).then((result) => {
-            data.role = result.data.role;
-            login(data);
-            history.push(`/${data.role}/`);
+        AgencyClient.get()
+          .then((result) => {
+            localStorage.setItem("agencyId", result.data.id);
+            data.agencyId = result.data.id;
+            data.userId = sub;
+            UserClient.getCurrentUser()
+              .then((result) => {
+                data.role = role;
+                login(data);
+                const path = role === "Admin" ? "admin" : "user";
+                history.push(`/${path}/`);
+              })
+              .catch((error) => {
+                setError(error, setErrors);
+              });
           })
           .catch((error) => {
             setError(error, setErrors);
           });
-        })
-        .catch((error) => {
-          setError(error, setErrors);
-        });
       })
       .catch((error) => {
         setError(error, setErrors);
@@ -88,78 +95,88 @@ const Login = (props) => {
             <Formik
               initialValues={{ username: "", password: "", message: "" }}
               validationSchema={LoginSchema}
-              onSubmit={(values, { setSubmitting, setErrors }) => { onSubmit(values, login, setSubmitting, setErrors) }}
+              onSubmit={(values, { setSubmitting, setErrors }) => {
+                onSubmit(values, login, setSubmitting, setErrors);
+              }}
             >
-            {({
-              values,
-              errors,
-              handleChange,
-              handleBlur,
-              handleSubmit,
-              isSubmitting,
-            }) => (
-              <form className={classes.form} onSubmit={handleSubmit}>
-                <Grid container direction="column" justify="center" alignItems="center">
-                  <Grid item>
-                    <Box>
-                      {"Don't have an account?"}
-                    </Box>
+              {({
+                values,
+                errors,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+              }) => (
+                <form className={classes.form} onSubmit={handleSubmit}>
+                  <Grid
+                    container
+                    direction="column"
+                    justify="center"
+                    alignItems="center"
+                  >
+                    <Grid item>
+                      <Box>{"Don't have an account?"}</Box>
+                    </Grid>
+                    <Grid item>
+                      <Link href="/registration" variant="body2">
+                        {"Register your travel agency here"}
+                      </Link>
+                    </Grid>
                   </Grid>
-                  <Grid item>
-                    <Link href="/registration" variant="body2">
-                      {"Register your travel agency here"}
-                    </Link>
+                  <FormikInputField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="username"
+                    label="Email Address"
+                    name="username"
+                    value={values.username}
+                    autoComplete="email"
+                    autoFocus
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                  />
+                  <FormikInputField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    value={values.password}
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                  />
+                  <Box
+                    display={
+                      errors.message || props?.location?.message ? "" : "none"
+                    }
+                    color={errors.message ? "error.main" : "success.main"}
+                  >
+                    {errors.message ? errors.message : props?.location?.message}
+                  </Box>
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}
+                    disabled={isSubmitting}
+                  >
+                    Sign In
+                  </Button>
+                  <Grid container justify="center" alignItems="center">
+                    <Grid item>
+                      <Link href="/forgotpassword" variant="body2">
+                        Forgot password?
+                      </Link>
+                    </Grid>
                   </Grid>
-                </Grid>
-                <FormikInputField
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="username"
-                  label="Email Address"
-                  name="username"
-                  value={values.username}
-                  autoComplete="email"
-                  autoFocus
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                />
-                <FormikInputField
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="password"
-                  value={values.password}
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                />
-                <Box display={errors.message || props?.location?.message ? "" : "none"} color={errors.message ? "error.main" : "success.main" }>
-                  {errors.message ? errors.message : props?.location?.message}
-                </Box>
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="primary"
-                  className={classes.submit}
-                  disabled={isSubmitting}
-                >
-                  Sign In
-                </Button>
-                <Grid container justify="center" alignItems="center">
-                  <Grid item>
-                    <Link href="/forgotpassword" variant="body2">
-                      Forgot password?
-                    </Link>
-                  </Grid>
-                </Grid>
-              </form>
-            )}
-          </Formik>
+                </form>
+              )}
+            </Formik>
           </LoginLayout>
         );
       }}
