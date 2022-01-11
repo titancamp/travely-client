3edf1bcd0b091fa01b1c3ea06c4f827fed96b613
Ani Naslyan <ani.naslyan@gmail.com>
2022-01-11 22:21:07 +0400

    FIN-15 Row edit sidebar some changes

--- src/src_V2/modules/manager/financial/components/payables/edit/drawer-content/notes/Notes.jsx
+++ src/src_V2/modules/manager/financial/components/payables/edit/drawer-content/notes/Notes.jsx
@@ -3,7 +3,6 @@ import {
   AccordionDetails,
   AccordionSummary,
   Box,
-  Button,
   TextField,
   Typography,
 } from '@mui/material';
@@ -33,18 +32,17 @@ export default function Notes({ row }) {
       </AccordionSummary>
       <AccordionDetails className={commonStyles.accordionDetails}>
         <TextField
-          placeholder="Type in..."
           minRows={3}
           multiline
           className={styles.notesTextArea}
           value={row.notes}
           fullWidth
+          label="Type in"
+          variant="outlined"
           InputProps={{
             endAdornment: (
-              <Box className={styles.saveSection}>
-                <Button variant="contained" className={styles.saveBtn}>
-                  Save Note
-                </Button>
+              <Box className={styles.lengthSection}>
+                <p>{row.notes?.length}/200 letter</p>
               </Box>
             ),
           }}
