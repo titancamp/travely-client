7120fa80eadbc4078642c38128136c63c10522ad
igortokhunts <igortokhunts@gmail.com>
2022-01-26 23:07:07 +0400

    [Tm-5] - Calendar integration, added custom styles/logique.

---
+++ src/src_V2/modules/manager/tour-management/tour-packages/add/calendar/constants.js
@@ -0,0 +1,84 @@
+import { ViewTypes } from 'react-big-scheduler';
+
+export const ViewsConfig = {
+  minuteStep: 60,
+  dayCellWidth: 120,
+  tableHeaderHeight: 30,
+  eventItemHeight: 22,
+  eventItemLineHeight: 30,
+  selectedAreaColor: 'red',
+  // nonAgendaSlotMinHeight: 0,
+  // schedulerWidth: '100%',
+  nonWorkingTimeHeadBgColor: 'white',
+  nonWorkingTimeBodyBgColor: 'white',
+  nonAgendaDayCellHeaderFormat: 'HH:mm',
+  summaryColor: 'red',
+
+  // headerEnabled: false,
+  // dayResourceTableWidth: 0,
+  views: [
+    {
+      viewName: 'Day',
+      viewType: ViewTypes.Day,
+    },
+    {
+      viewName: 'Week',
+      viewType: ViewTypes.Week,
+      showAgenda: false,
+      isEventPerspective: false,
+    },
+    {
+      viewName: 'Month',
+      viewType: ViewTypes.Month,
+    },
+  ],
+};
+
+export const SchedulerInitialData = {
+  resources: [
+    {
+      id: 'accommodation',
+      name: '',
+    },
+    {
+      id: 'food',
+      name: '',
+    },
+    {
+      id: 'transportation',
+      name: '',
+    },
+    {
+      id: 'guide',
+      name: '',
+    },
+    {
+      id: 'activity',
+      name: '',
+    },
+  ],
+  events: [],
+};
+
+export const eventColorStyles = {
+  accommodation: {
+    border: 'rgba(248, 128, 120, 0.3)',
+    background: '#FEF2F1',
+  },
+  food: {
+    border: 'rgba(255, 181, 71, 0.4)',
+    background: '#FFF8ED',
+  },
+  transportation: {
+    border: 'rgba(100, 182, 247, 0.3)',
+    background: '#F0F8FE',
+  },
+  guide: {
+    border: 'rgba(123, 198, 126, 0.3)',
+    background: '#EBF6EC',
+  },
+  activity: {
+    border: 'rgba(226, 130, 228, 0.4)',
+    background: '#FCF3FC',
+  },
+};
---
+++ src/src_V2/modules/manager/tour-management/tour-packages/add/calendar/helper.js
@@ -0,0 +1,109 @@
+import { useState } from 'react';
+import { SchedulerData, ViewTypes } from 'react-big-scheduler';
+
+import { SchedulerInitialData, ViewsConfig, eventColorStyles } from './constants';
+
+export function getInitialScheduler() {
+  if (!getInitialScheduler.scheduler) {
+    let scheduler = new SchedulerData(
+      new Date(),
+      ViewTypes.Day,
+      false,
+      false,
+      ViewsConfig
+    );
+    scheduler.localeMoment.locale('en');
+    scheduler.setEvents(SchedulerInitialData.events);
+    scheduler.setResources(SchedulerInitialData.resources);
+
+    getInitialScheduler.scheduler = scheduler;
+  }
+
+  return getInitialScheduler.scheduroot;
+}
+
+export function useSchedulerHandlers(scheduler) {
+  const [viewModel, setViewModel] = useState({ schedulerData: scheduler, toggle: false });
+
+  function newEvent(schedulerData, slotId, slotName, start, end, type, item) {
+    let newFreshId = 0;
+    schedulerData.events.forEach((item) => {
+      if (item.id >= newFreshId) newFreshId = item.id + 1;
+    });
+
+    let newEvent = {
+      id: newFreshId,
+      title: 'New event you just created',
+      start: start,
+      end: end,
+      resourceId: slotId,
+    };
+
+    schedulerData.addEvent(newEvent);
+    setViewModel({ schedulerData, toggle: !viewModel.toggle });
+  }
+
+  function eventItemTemplateResolver(schedulerData, event, mustAddCssClass, a, b) {
+    let titleText = schedulerData.behaviors.getEventTextFunc(schedulerData, event);
+    const { border, background } = eventColorStyles[event.resourceId];
+
+    return (
+      <div
+        key={event.id}
+        style={{
+          cursor: 'default',
+          borderRadius: '3px',
+          marginBottom: '3px',
+          backgroundColor: background,
+          border: `1px solid ${border}`,
+          boxShadow: '0px 2px 1px -1px rgba(0, 0, 0, 0.1)',
+        }}
+      >
+        <span style={{ color: '#FFFFFF', margin: '4px 0 0 2px', borderRadius: 1 }}>
+          1{/*{titleText}*/}
+        </span>
+      </div>
+    );
+  }
+
+  function onViewChange(schedulerData, view) {
+    schedulerData.setViewType(view.viewType, view.showAgenda, view.isEventPerspective);
+    schedulerData.setEvents(schedulerData.events);
+    setViewModel({ schedulerData, toggle: !viewModel.toggle });
+  }
+
+  function onSelectDate(schedulerData, date) {
+    schedulerData.setDate(date);
+    schedulerData.setEvents(schedulerData.events);
+    setViewModel({ schedulerData, toggle: !viewModel.toggle });
+  }
+
+  function updateEventStart(schedulerData, event, newStart, a, b) {
+    schedulerData.updateEventStart(event, newStart);
+    setViewModel({ schedulerData, toggle: !viewModel.toggle });
+  }
+
+  function updateEventEnd(schedulerData, event, newEnd) {
+    schedulerData.updateEventEnd(event, newEnd);
+    setViewModel({ schedulerData, toggle: !viewModel.toggle });
+  }
+
+  function moveEvent(schedulerData, event, slotId, slotName, start, end) {
+    schedulerData.moveEvent(event, slotId, slotName, start, end);
+    setViewModel({ schedulerData, toggle: !viewModel.toggle });
+  }
+
+  function conflictOccurred() {}
+
+  return {
+    viewModel,
+    newEvent,
+    moveEvent,
+    onViewChange,
+    onSelectDate,
+    updateEventEnd,
+    updateEventStart,
+    conflictOccurred,
+    eventItemTemplateResolver,
+  };
+}
